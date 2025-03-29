ea461139378a42d2e14fa49ed90ad6a3712a8fa4
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2025-03-29 22:28:10 +0530

    Added: GraphQL helpers

---
+++ backend/graphql/resolvers.js
@@ -0,0 +1,33 @@
+const Book = require("../models/Book");
+const User = require("../models/User");
+
+const resolvers = {
+	Query: {
+		books: async () => await Book.find(),
+		book: async (_, { id }) => await Book.findById(id),
+		users: async () => await User.find(),
+		user: async (_, { id }) => await User.findById(id),
+	},
+	Mutation: {
+		addBook: async (
+			_,
+			{ title, author, genre, language, description, buyLink }
+		) => {
+			const newBook = new Book({
+				title,
+				author,
+				genre,
+				language,
+				description,
+				buyLink,
+			});
+			return await newBook.save();
+		},
+		addUser: async (_, { username, email }) => {
+			const newUser = new User({ username, email });
+			return await newUser.save();
+		},
+	},
+};
+
+module.exports = resolvers;
---
+++ backend/graphql/schema.js
@@ -0,0 +1,42 @@
+const { gql } = require("graphql-tag");
+
+const typeDefs = gql`
+	type Book {
+		id: ID!
+		title: String!
+		author: String!
+		genre: String!
+		language: String!
+		rating: Float
+		description: String
+		buyLink: String
+	}
+
+	type User {
+		id: ID!
+		username: String!
+		email: String!
+		favoriteBooks: [Book]
+	}
+
+	type Query {
+		books: [Book]
+		book(id: ID!): Book
+		users: [User]
+		user(id: ID!): User
+	}
+
+	type Mutation {
+		addBook(
+			title: String!
+			author: String!
+			genre: String!
+			language: String!
+			description: String
+			buyLink: String
+		): Book
+		addUser(username: String!, email: String!): User
+	}
+`;
+
+module.exports = typeDefs;
