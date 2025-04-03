23c06955785700c13bc526b2f1158d308e635882
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2025-04-03 22:43:07 +0530

    WIP

--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const userSchema = new Schema({
-	username: { type: String, required: true },
-	email: { type: String, required: true, unique: true },
-	favoriteBooks: [{ type: Schema.Types.ObjectId, ref: "Book" }],
-});
-
-export default model("User", userSchema);
